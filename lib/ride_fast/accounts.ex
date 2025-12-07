69911f7dd35c6caf8d742265d31d048ebb91626f
gbrlevi <powerzinns@gmail.com>
2025-12-07 20:10:47 -0300

    fix: users delete

--- lib/ride_fast/accounts.ex
+++ lib/ride_fast/accounts.ex
@@ -5,6 +5,7 @@ defmodule RideFast.Accounts do
 
   import Ecto.Query, warn: false
   alias RideFast.Repo
+
   alias RideFast.Accounts.User
   alias RideFast.Global.Language
   alias RideFast.Global
@@ -12,48 +13,25 @@ defmodule RideFast.Accounts do
 
   @doc """
   Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
   """
   def list_users do
-    Repo.all(User)
+    User
+    |> where([u], is_nil(u.deleted_at))
+    |> Repo.all()
   end
 
   @doc """
   Gets a single user.
-
   Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
   """
   def get_user!(id) do
     User
     |> where([u], is_nil(u.deleted_at))
-    |> Repo.get(id)
+    |> Repo.get!(id)
   end
 
   @doc """
   Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_user(attrs) do
     %User{}
@@ -63,15 +41,6 @@ defmodule RideFast.Accounts do
 
   @doc """
   Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -80,29 +49,14 @@ defmodule RideFast.Accounts do
   end
 
   @doc """
-  Deletes a user.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
+  Deletes a user (Soft Delete).
   """
   def delete_user(%User{} = user) do
-    Repo.delete(user)
+    soft_delete_user(user)
   end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
@@ -112,12 +66,6 @@ defmodule RideFast.Accounts do
 
   @doc """
   Returns the list of drivers.
-
-  ## Examples
-
-      iex> list_drivers()
-      [%Driver{}, ...]
-
   """
   def list_drivers do
     Repo.all(Driver)
@@ -125,31 +73,11 @@ defmodule RideFast.Accounts do
 
   @doc """
   Gets a single driver.
-
-  Raises `Ecto.NoResultsError` if the Driver does not exist.
-
-  ## Examples
-
-      iex> get_driver!(123)
-      %Driver{}
-
-      iex> get_driver!(456)
-      ** (Ecto.NoResultsError)
-
   """
   def get_driver!(id), do: Repo.get!(Driver, id)
 
   @doc """
   Creates a driver.
-
-  ## Examples
-
-      iex> create_driver(%{field: value})
-      {:ok, %Driver{}}
-
-      iex> create_driver(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_driver(attrs) do
     %Driver{}
@@ -159,15 +87,6 @@ defmodule RideFast.Accounts do
 
   @doc """
   Updates a driver.
-
-  ## Examples
-
-      iex> update_driver(driver, %{field: new_value})
-      {:ok, %Driver{}}
-
-      iex> update_driver(driver, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_driver(%Driver{} = driver, attrs) do
     driver
@@ -177,15 +96,6 @@ defmodule RideFast.Accounts do
 
   @doc """
   Deletes a driver.
-
-  ## Examples
-
-      iex> delete_driver(driver)
-      {:ok, %Driver{}}
-
-      iex> delete_driver(driver)
-      {:error, %Ecto.Changeset{}}
-
   """
   def delete_driver(%Driver{} = driver) do
     Repo.delete(driver)
@@ -193,12 +103,6 @@ defmodule RideFast.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking driver changes.
-
-  ## Examples
-
-      iex> change_driver(driver)
-      %Ecto.Changeset{data: %Driver{}}
-
   """
   def change_driver(%Driver{} = driver, attrs \\ %{}) do
     Driver.changeset(driver, attrs)
@@ -221,7 +125,9 @@ defmodule RideFast.Accounts do
   end
 
   def get_user_by_email(email) do
-    Repo.get_by(User, email: email)
+    User
+    |> where([u], is_nil(u.deleted_at))
+    |> Repo.get_by(email: email)
   end
 
   def get_driver_by_email(email) do
@@ -251,7 +157,7 @@ defmodule RideFast.Accounts do
 
     query = from u in User,
       where: u.role == :user,
-      where: is_nil(u.deleted_at)
+      where: is_nil(u.deleted_at) 
 
     query =
       if search_term && search_term != "" do
@@ -282,7 +188,6 @@ defmodule RideFast.Accounts do
 
   def soft_delete_user(%User{} = user) do
     now = NaiveDateTime.utc_now()
-
     truncated_now = NaiveDateTime.truncate(now, :second)
 
     user
@@ -290,30 +195,7 @@ defmodule RideFast.Accounts do
     |> Repo.update()
   end
 
-  def add_language_to_driver(driver_id, language_id) do
-    driver = Repo.get!(Driver, driver_id) |> Repo.preload(:languages)
-
-    language = Repo.get!(Language, language_id)
-
-    already_has? = Enum.any?(driver.languages, fn l -> l.id == language.id end)
-
-    if already_has? do
-      {:error, :conflict}
-    else
-      driver
-      |> Ecto.Changeset.change()
-      |> Ecto.Changeset.put_assoc(:languages, [language | driver.languages])
-      |> Repo.update()
-    end
-  end
-
-  def list_driver_languages(driver_id) do
-    driver = Repo.get!(Driver, driver_id)
-
-    driver = Repo.preload(driver, :languages)
-
-    driver.languages
-  end
+  # --- Funções de Profile e Idiomas ---
 
   def get_driver_profile_by_driver_id(driver_id) do
     Repo.get_by(DriverProfile, driver_id: driver_id)
@@ -331,9 +213,30 @@ defmodule RideFast.Accounts do
     |> Repo.update()
   end
 
+  def add_language_to_driver(driver_id, language_id) do
+    driver = Repo.get!(Driver, driver_id) |> Repo.preload(:languages)
+    language = Repo.get!(Language, language_id)
+
+    already_has? = Enum.any?(driver.languages, fn l -> l.id == language.id end)
+
+    if already_has? do
+      {:error, :conflict}
+    else
+      driver
+      |> Ecto.Changeset.change()
+      |> Ecto.Changeset.put_assoc(:languages, [language | driver.languages])
+      |> Repo.update()
+    end
+  end
+
+  def list_driver_languages(driver_id) do
+    driver = Repo.get!(Driver, driver_id)
+    driver = Repo.preload(driver, :languages)
+    driver.languages
+  end
+
   def remove_language_from_driver(driver_id, language_id) do
     driver = Repo.get!(Driver, driver_id) |> Repo.preload(:languages)
-
     target_id = String.to_integer(language_id)
 
     exists? = Enum.any?(driver.languages, fn l -> l.id == target_id end)
@@ -349,6 +252,4 @@ defmodule RideFast.Accounts do
       {:error, :not_found}
     end
   end
-
-
 end
